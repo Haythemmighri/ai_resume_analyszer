8fd763b896939de0668bd70d577b3f00e812c907
Haythemmighri <mgh.haythem@gmail.com>
2025-11-28 20:26:24 +0100

    commit for friday

---
+++ app/components/navbar.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router"
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+        <Link to="/">
+            <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+        </Link>
+        <Link to="/upload" className="primary-button w-fit">
+            upload my resume
+        </Link>
+    </nav>
+  )
+}
+
+export default Navbar
